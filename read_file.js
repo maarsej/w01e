9af288da1e03b8339741c46372fe31ec5aa5e023
Nucleartom <jacob.maarse@gmail.com>
2018-03-15 18:38:04 +0000

    first commit

---
+++ read_file.js
@@ -0,0 +1,8 @@
+
+var fs = require('fs')
+  , filename = process.argv[2];
+fs.readFile(filename, 'utf8', function(err, data) {
+  if (err) throw err;
+  //console.log('OK: ' + filename);
+  console.log(data);
+});
